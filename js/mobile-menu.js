42fde9255fbc97ffd737d7ebf4038c7ebcbe689f
ladida69 <vlad.kharytiuk@gmail.com>
2025-07-19 13:22:15 +0200

    final check up v2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,14 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileMenuScroll = document.querySelector('[data-no-scroll]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
-    document.body.classList.add('modal-open'); // коли модалка відкрита
-    document.body.classList.remove('modal-open'); // коли модалка закрита
     mobileMenuRef.classList.toggle('is-open');
 
-    mobileMenuScroll.classList.toggle('no-scroll');
+    document.body.classList.toggle('no-scroll');
   });
 })();
